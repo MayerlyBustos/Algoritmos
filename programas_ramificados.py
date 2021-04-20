2df9e1ef4026b1c354e707a8ec9d122169bdb5a7
Mayerly Bustos <bustosrobayom@gmail.com>
2021-04-20 11:10:47 -0500

    Programas ramificados en python

---
+++ programas_ramificados.py
@@ -0,0 +1,9 @@
+num_1 = int(input('Ingresa un numero: '))
+num_2 = int(input('Ingresa otro numero'))
+
+if num_1 > num_2:
+    print('El primero numero es mayor que el segundo')
+elif num_1 < num_2:
+    print('El segundo numero es mayor que el primero')
+else:
+    print('Los numero son iguales')
