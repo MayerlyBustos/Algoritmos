4a80332d4a694b43f2e4eb8784338ff03d60c58d
Mayerly Bustos <bustosrobayom@gmail.com>
2021-04-21 21:37:37 -0500

    Aproximacion

---
+++ aproximacion.py
@@ -0,0 +1,13 @@
+objetivo = int(input('Ingresa un numero: '))
+epsilon = 0.001
+paso = epsilon**2
+respuesta = 0.0
+
+while abs(respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo:
+  print(abs(respuesta**2 - objetivo))
+  respuesta += paso
+
+if abs(respuesta**2 - objetivo) >= epsilon:  
+  print(f'No se encontro raiz cuadrada {objetivo}')
+else:
+  print(f'La raiz cuadrada de {objetivo} es {respuesta}')
